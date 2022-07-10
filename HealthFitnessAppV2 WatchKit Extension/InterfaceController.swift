c6fb8f56d0288a5717c0a3af93574c616fd49608
Zachary Bryce Ellis <zbellis03@gmail.com>
2022-07-10 14:18:51 -0500

    Set up UI, and app can take user heart rate, and print to screen

--- HealthFitnessAppV2 WatchKit Extension/InterfaceController.swift
+++ HealthFitnessAppV2 WatchKit Extension/InterfaceController.swift
@@ -7,13 +7,37 @@
 
 import WatchKit
 import Foundation
-
+import HealthKit
 
 class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet weak var currentHRLbl: WKInterfaceLabel!
+    var heartRateMeasurementService = HeartRateMeasurementService()
+    
+   
+    @IBAction func runHRStats() {
+        _ = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { [self] timer in
+            
+            currentHRLbl.setText(String(heartRateMeasurementService.currentHeartRate))
+//            hrLbl.setText(String(heartRateMeasurementService.currentHeartRate))
+//            maxHrLbl.setText(String(heartRateMeasurementService.maxHeartRate))
+//            minHrLbl.setText(String(heartRateMeasurementService.minHeartRate))
 
+            print("HR Triggered")
+//
+//            if heartRateMeasurementService.currentHeartRate < 85 {
+//                staminaBarImg.setImageNamed("StaminaBar")
+//            } else if heartRateMeasurementService.currentHeartRate < 100 {
+//                staminaBarImg.setImageNamed("StaminaBar80")
+//            } else  {
+//                staminaBarImg.setImageNamed("StaminaBar60")
+//            }
+        }
+    }
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
-        print("Hello world2")
+
     }
     
     override func willActivate() {
